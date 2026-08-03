284f582aca2509e957c61a4c388f13195a36f453
OEM <OEM@DESKTOP-HFTGBQ5>
2026-08-03 22:03:53 +0200

    Configure Swagger UI and API documentation

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,17 +1,45 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// builder.Services.AddOpenApi();
+
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Sentence Builder API",
+        Version = "v1",
+        Description = "An API for building sentences from words.",
+        Contact = new OpenApiContact
+        {
+            Name = "Nhlamulo Reply Shikweni",
+            Email = "shikweninhlamulo@example.com",
+            Url = new Uri("https://github.com/Nhlamulo-Reply/sentence-builder-project.git")
+        }
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI( options =>
+    {
+        options.DocumentTitle = "Sentence Builder API";
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+    // app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
---
+++ Changelog.md
@@ -0,0 +1,32 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## [unreleased]
+
+### Added
+ - Project Structure
+ - ASP.NET core Web API
+ - Added Angular application
+
+
+ ### [0.0.1] - 2026-08-03
+
+ ## Added 
+ - Initialized git repository
+ - Created backend project structure
+ - Created Angular Frontend
+ - Added root gitignore
+ - Configure project folder structure
+ - Configured Swagger for Api documentation
+
+
+ ### Fix
+
+ ### Modify
+
+
+ ### Remove
+
+ - Deleted default weatherforecast api
+ - Removed OpenAPI to use swagger for Api documentation
