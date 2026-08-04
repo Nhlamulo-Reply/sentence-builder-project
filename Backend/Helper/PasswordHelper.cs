5c06150c861463ca26f8eb9024f0b4a38065d0cf
OEM <OEM@DESKTOP-HFTGBQ5>
2026-08-04 23:37:27 +0200

    Added passwordhelper class

---
+++ Backend/Helper/PasswordHelper.cs
@@ -0,0 +1,21 @@
+﻿using Backend.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Backend.Helper;
+
+public static class PasswordHelper
+{
+    private static readonly PasswordHasher<User> _passwordHasher = new();
+
+    public static string HashPassword(string password)
+    {
+        return _passwordHasher.HashPassword(new User(), password);
+    }
+
+    public static bool VerifyPassword(User user, string password, string passwordHash)
+    {
+        var result = _passwordHasher.VerifyHashedPassword(user, passwordHash, password);
+
+        return result == PasswordVerificationResult.Success;
+    }
+}
