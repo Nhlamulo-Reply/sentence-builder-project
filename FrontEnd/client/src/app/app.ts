4e1a64e29cc57dd153cb0c673f3681845d524918
OEM <OEM@DESKTOP-HFTGBQ5>
2026-08-05 08:56:10 +0200

    Added environment variables

--- FrontEnd/client/src/app/app.ts
+++ FrontEnd/client/src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Login} from './features/auth/login/login';
+import {Dashboard} from './features/dashboard/dashboard/dashboard';
+import {Navbar} from './core/layout/navbar/navbar';
+import {Builder} from './features/sentence-builder/builder/builder';
+import {History} from './features/sentence-builder/history/history';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login,Navbar],
   templateUrl: './app.html',
+  standalone: true,
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('client');
+  protected readonly title = signal('LexiCraft');
 }
