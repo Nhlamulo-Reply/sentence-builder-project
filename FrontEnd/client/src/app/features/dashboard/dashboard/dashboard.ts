46260266a8c16982fc14f45b121bda40ee397dc6
OEM <OEM@DESKTOP-HFTGBQ5>
2026-08-07 08:56:01 +0200

    Improved welcome page or dashboard page

--- FrontEnd/client/src/app/features/dashboard/dashboard/dashboard.ts
+++ FrontEnd/client/src/app/features/dashboard/dashboard/dashboard.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {AccountService} from '../../../core/services/account-service';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
   standalone: true
 })
-export class Dashboard {}
+export class Dashboard implements OnInit
+{
+  private accountService = inject(AccountService);
+
+  username = 'User';
+
+  ngOnInit(): void
+  {
+    const user = this.accountService.getUser();
+    this.username = user?.username || user?.email || 'User';
+  }
+
+}
