e5a5e30d07ccc96f5750a8c7477d1d59b2b80595
OEM <OEM@DESKTOP-HFTGBQ5>
2026-08-10 10:23:42 +0200

    Edit component fix

--- FrontEnd/client/src/app/core/services/word-type-service.ts
+++ FrontEnd/client/src/app/core/services/word-type-service.ts
@@ -1,6 +1,8 @@
 import {inject, Injectable} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {environment} from '../../../environments/environment.development';
+import {Word, WordType} from '../Models/Word';
+
 
 @Injectable({
   providedIn: 'root',
@@ -8,19 +10,16 @@ import {environment} from '../../../environments/environment.development';
 export class WordTypeService {
 
   private http = inject(HttpClient)
-
   private baseApiUrl = environment.apiUrl;
 
-
-  getAllWordsType(){
-    return  this.http.get(`${this.baseApiUrl}WordType`);
+  getAllWordsType()
+  {
+    return  this.http.get<WordType[]>(`${this.baseApiUrl}WordType`);
   }
-
-
   getAllWordByIdType(wordTypeId: number)
-{
-  return this.http.get(`${this.baseApiUrl}Word/wordtype/${wordTypeId}`);
-}
+  {
+    return this.http.get<Word[]>(`${this.baseApiUrl}Word/wordtype/${wordTypeId}`);
+  }
 
 
 
