543be149be6da9d1b3241183e29188cb76da8035
OEM <OEM@DESKTOP-HFTGBQ5>
2026-08-23 18:57:30 +0200

    added auth guard for protecting routes

--- FrontEnd/client/src/app/app.routes.ts
+++ FrontEnd/client/src/app/app.routes.ts
@@ -4,12 +4,13 @@ import {Dashboard} from './features/dashboard/dashboard/dashboard';
 import {Navbar} from './core/layout/navbar/navbar';
 import {Builder} from './features/sentence-builder/builder/builder';
 import {History} from './features/sentence-builder/history/history';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: Login },
-  { path: 'dashboard', component: Dashboard },
-  { path: 'builder', component: Builder },
-  { path: 'history', component: History },
+  { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
+  { path: 'builder', component: Builder, canActivate: [authGuard] },
+  { path: 'history', component: History, canActivate: [authGuard] },
 
 ];
